2de9931e1a0d65f85dc3ea5481307546f7563a93
MininDS <MininDS178@gmail.com>
2022-07-01 02:50:27 +0300

    Fully refactored code - added huge fixture with all user-action-methods in different class.

---
+++ application.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class Application:
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def open_main_page(self):
+        # Open main page
+        wd = self.wd
+        wd.get("http://localhost:8080/addressbook/group.php")
+
+    def login(self, username, password):
+        # Login
+        wd = self.wd
+        self.open_main_page()
+
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def init_group_creation(self):
+        # Open groups page and init new group creation
+        wd = self.wd
+        wd.find_element(By.NAME, "new").click()
+
+    def create_group(self, group):
+        # Fill group form
+        wd = self.wd
+        self.init_group_creation()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # Submit group creation
+        wd.find_element(By.NAME, "submit").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        # Return to groups page
+        wd = self.wd
+        wd.find_element(By.XPATH, "//*[@id='content']/div/i/a").click()
+
+    def logout(self):
+        # Logout
+        wd = self.wd
+        wd.find_element(By.XPATH, "//*[@id='top']/form/a").click()
+
+    def destroy(self):
+        self.wd.quit()
--- test_add_group_Chrome.py
+++ test_add_group_Chrome.py
@@ -1,101 +1,26 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+
+
 from group import Group
-
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        # Private method for selected browser start and implicitly wait pause for exception
-        self.type_of_browser = 'Chrome'
-
-        if self.type_of_browser == 'Chrome':
-            self.wd = webdriver.Chrome()
-        elif self.type_of_browser == 'Firefox':
-            self.wd = webdriver.Firefox()
-
-        self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
-        # Main test scenario - open site, login, create group, return to groups page and logout
-
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="Test", header="testtest", footer="testtesttest"))
-        self.logout()
-
-    def test_add_empty_group(self):
-        # Slave test scenario - open site, login, create group with empty forms, return to groups page and logout
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-    def open_main_page(self):
-        # Open main page
-        wd = self.wd
-        wd.get("http://localhost:8080/addressbook/group.php")
-
-    def login(self, username, password):
-        # Login
-        wd = self.wd
-        self.open_main_page()
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def init_group_creation(self):
-        # Open groups page and init new group creation
-        wd = self.wd
-        wd.find_element(By.NAME, "new").click()
-
-    def create_group(self, group):
-        # Fill group form
-        wd = self.wd
-        self.init_group_creation()
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # Submit group creation
-        wd.find_element(By.NAME, "submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        # Return to groups page
-        wd = self.wd
-        wd.find_element(By.XPATH, "//*[@id='content']/div/i/a").click()
-
-    def logout(self):
-        # Logout
-        wd = self.wd
-        wd.find_element(By.XPATH, "//*[@id='top']/form/a").click()
-
-    def is_element_present(self, how, what):
-        # Private method for definition of elements' presentation on needed page
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        # Private method for alert when element is not on a page
-        try: self.wd.switch_to.alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    def tearDown(self):
-        # Private method - closes browser window
-        self.wd.quit()
+from application import Application
+import pytest
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    # Main test scenario - open site, login, create group, return to groups page and logout
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Test", header="testtest", footer="testtesttest"))
+    app.logout()
+
+def test_add_empty_group(app):
+    # Slave test scenario - open site, login, create group with empty forms, return to groups page and logout
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
