3573d1bbde1ff7b0ee09a69ff49baa521bc4c76c
MininDS <MininDS178@gmail.com>
2022-07-13 01:43:23 +0300

    Created new class-helper for contacts-management - some methods for contacts create operations.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from contact import Contact
+from models.contact import Contact
 from fixtures.application import Application
 import pytest
 
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_contact(app):
     # Main test scenario - open site, login, create contact, return to main page and logout
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="test", middle_name="test", last_name="test", nickname="test",
-                                        title="test", company="test", address="test", home_phone="test", mobile_phone="test",
-                                        work_phone="test", fax="test", email1="test", email2="test", email3="test",
-                                        homepage="test", birthday_day="1", birthday_month="January",
-                                        birthday_year="1900", anniversary_day="1", anniversary_month="February",
-                                        anniversary_year="2000", address2="test", home2="test", notes="test"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(first_name="test", middle_name="test", last_name="test", nickname="test",
+                               title="test", company="test", address="test", home_phone="test", mobile_phone="test",
+                               work_phone="test", fax="test", email1="test", email2="test", email3="test",
+                               homepage="test", birthday_day="1", birthday_month="January",
+                               birthday_year="1900", anniversary_day="1", anniversary_month="February",
+                               anniversary_year="2000", address2="test", home2="test", notes="test"))
+    app.session.logout()
