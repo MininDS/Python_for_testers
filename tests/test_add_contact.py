dd5026d933690c2c4186604310dccd4b779077e2
MininDS <MininDS178@gmail.com>
2022-07-13 01:02:46 +0300

    Created packages for different responsibility zones.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from contact import Contact
-from application import Application
+from fixtures.application import Application
 import pytest
 
 
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from group import Group
-from application import Application
+from models.group import Group
+from fixtures.application import Application
 import pytest
 
 
