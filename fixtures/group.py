cca26736fdb19dacaa06a57586edd9355c049600
MininDS <MininDS178@gmail.com>
2022-07-24 00:58:31 +0300

    Extracted general method in group-helper class for filling group form for create and edit operations.

--- fixtures/group.py
+++ fixtures/group.py
@@ -15,13 +15,8 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element(By.NAME, "new").click()
 
-    def create(self, group):
-        # Fill group form
+    def fill_group_form_full(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # Begin creation process - open group page for poles filling
-        self.init_group_creation()
-        # Fill poles of group name, header and footer
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
         wd.find_element(By.NAME, "group_name").send_keys(group.name)
@@ -31,6 +26,15 @@ class GroupHelper:
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        # Fill group form
+        wd = self.app.wd
+        self.open_groups_page()
+        # Begin creation process - open group page for poles filling
+        self.init_group_creation()
+        # Fill poles of group name, header and footer
+        self.fill_group_form_full(group)
         # Submit group creation
         wd.find_element(By.NAME, "submit").click()
         # Return to groups page again
@@ -49,15 +53,7 @@ class GroupHelper:
         # Begin modification process - open selected group for poles modification
         self.init_group_edit()
         # Modify all poles of group name, header and footer
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        self.fill_group_form_full(group)
         # Submit group modification
         wd.find_element(By.NAME, "update").click()
         # Return to groups page again
